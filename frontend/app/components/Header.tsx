a6a9b29ae3db02128c49784db7cf532ec22d36b9
ndizeyedavid <davidndizeye101@gmail.com>
2026-04-13 09:53:46 +0200

    ft: intial setup for the dashboard and landing page

---
+++ frontend/app/components/Header.tsx
@@ -0,0 +1,20 @@
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <div>{/* Logo here */}</div>
+
+        <nav>
+          <a href="">Find Jobs</a>
+          <a href="">About</a>
+          <a href="">Contact</a>
+        </nav>
+      </div>
+
+      <div>
+        <button>Login</button>
+        <button>Sign Up</button>
+      </div>
+    </header>
+  );
+}
